df620af60f30a58968ff46fcbf3dfae0a9a243bb
bagusakti <142561975+bagusakti@users.noreply.github.com>
2023-12-28 13:54:02 +0700

    tampilanhome

---
+++ resources/views/web/partials/navbar.blade.php
@@ -0,0 +1,143 @@
+<header>
+    <div class="headerarea headerarea__3 header__sticky header__area">
+        <div class="container desktop__menu__wrapper">
+            <div class="row">
+                <div class="col-xl-2 col-lg-2 col-md-6">
+                    <div class="headerarea__left">
+                        <div class="headerarea__left__logo">
+
+                            <a href="index.html"><img src="img/logo/logo_1.png" alt="logo"></a>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-xl-7 col-lg-7 main_menu_wrap">
+                    <div class="headerarea__main__menu">
+                        <nav>
+                            <ul>
+                                <li class="mega__menu position-static">
+                                    <a class="headerarea__has__dropdown" href="">Home<i class="icofont-rounded"></i></a>
+                                </li>
+                                <li class="mega__menu position-static">
+                                    <a class="headerarea__has__dropdown" href="">Pages<i class="icofont-rounded"></i> </a>
+                                </li>
+                                <li class="mega__menu position-static">
+                                    <a class="headerarea__has__dropdown" href="">Courses<i class="icofont-rounded"></i> </a>
+                                </li>
+                                <li>
+                                    <a class="headerarea__has__dropdown" href="">Dashboard<i class="icofont-rounded"></i></a>
+                                </li>
+                                <li>
+                                <a class="headerarea__has__dropdown" href="">eCommerce<i class="icofont-rounded"></i></a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6">
+                    <div class="headerarea__right">
+                        <div class="headerarea__login">
+                            <a href=""><i class="icofont-user-alt-5"></i></a>
+                        </div>
+                        <div class="headerarea__button">
+                            <a href="">Masuk</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+        <div class="container-fluid mob_menu_wrapper">
+            <div class="row align-items-center">
+                <div class="col-6">
+                    <div class="mobile-logo">
+                        <a class="logo__dark" href="#"><img src="img/logo/logo_1.png" alt="logo"></a>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="header-right-wrap">
+
+                        <div class="headerarea__right">
+
+                            <div class="header__cart">
+                                <a href="#"> <i class="icofont-cart-alt"></i></a>
+                                <div class="header__right__dropdown__wrapper">
+                                    <div class="header__right__dropdown__inner">
+                                        <div class="single__header__right__dropdown">
+
+                                            <div class="header__right__dropdown__img">
+                                                <a href="#">
+                                                    <img src="img/grid/cart1.jpg" alt="photo">
+                                                </a>
+                                            </div>
+                                            <div class="header__right__dropdown__content">
+
+                                                <a href="shop-product.html">Web Directory</a>
+                                                <p>1 x <span class="price">$ 80.00</span></p>
+
+                                            </div>
+                                            <div class="header__right__dropdown__close">
+                                                <a href="#"><i class="icofont-close-line"></i></a>
+                                            </div>
+                                        </div>
+
+                                        <div class="single__header__right__dropdown">
+
+                                            <div class="header__right__dropdown__img">
+                                                <a href="#">
+                                                    <img src="img/grid/cart2.jpg" alt="photo">
+                                                </a>
+                                            </div>
+                                            <div class="header__right__dropdown__content">
+
+                                                <a href="shop-product.html">Design Minois</a>
+                                                <p>1 x <span class="price">$ 60.00</span></p>
+
+                                            </div>
+                                            <div class="header__right__dropdown__close">
+                                                <a href="#"><i class="icofont-close-line"></i></a>
+                                            </div>
+                                        </div>
+
+                                        <div class="single__header__right__dropdown">
+
+                                            <div class="header__right__dropdown__img">
+                                                <a href="#">
+                                                    <img src="img/grid/cart3.jpg" alt="photo">
+                                                </a>
+                                            </div>
+                                            <div class="header__right__dropdown__content">
+
+                                                <a href="shop-product.html">Crash Course</a>
+                                                <p>1 x <span class="price">$ 70.00</span></p>
+
+                                            </div>
+                                            <div class="header__right__dropdown__close">
+                                                <a href="#"><i class="icofont-close-line"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <p class="dropdown__price">Total: <span>$1,100.00</span>
+                                    </p>
+                                    <div class="header__right__dropdown__button">
+                                        <a href="#" class="white__color">VIEW
+                                    CART</a>
+                                        <a href="#" class="blue__color">CHECKOUT</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="mobile-off-canvas">
+                            <a class="mobile-aside-button" href="#"><i class="icofont-navigation-menu"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
