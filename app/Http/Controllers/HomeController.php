bd63d0fa152bc3de7f425ba36eb3ac0d96ac4173
bagusakti <142561975+bagusakti@users.noreply.github.com>
2024-01-09 00:06:38 +0700

    ToDeploy

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index() {
+        $user = Auth::user();
         return view('web.home.index', [
-            'title' => 'Home'
+            'title' => 'Home',
+            'users' => $user
         ]);
     }
 }
